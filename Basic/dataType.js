d8a69a7ce2bd7daa64bd967ed4d104ffb4094a45
Nikita-singh-14 <nk2371174@gmail.com>
2026-06-28 07:11:42 +0000

    day1.4

---
+++ Basic/dataType.js
@@ -0,0 +1,16 @@
+"use Strict"; //use newer version of js
+let name = "nikita";
+let age = 21;
+let city = null;
+let isLoggedIn = false;
+/*
+string => "", ''
+number => 2 power 53
+boolean => true / false
+null => standlone value => ye ek speacial value hai jo kisi bhi variable ko assign kiya ja sakta hai
+undefined => variable declared hai but value assign nhi hai
+symbol => uniqueness find karne me
+object
+*/
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
