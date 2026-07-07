7ee961f4c35af1c8b32ff6da01679387d2751e2c
Nikita-singh-14 <nk2371174@gmail.com>
2026-07-07 01:53:12 +0000

    Day8.1

---
+++ ControlFlow/first.js
@@ -0,0 +1,27 @@
+//!==, !=, ===, ==, <=, >=, 
+
+// const score = 200;
+// if(score > 100) console.log("test");
+
+// if(score < 100){
+//     console.log("less than 100")
+// }else if(score < 150){
+//     console.log("greater than 150")
+// }else{
+//     console.log("final")
+// }
+
+
+const userLogin = true;
+const debitCard = true;
+// if(userLogin && debitCard){
+//     console.log("Allow to buy course");
+    
+// }
+
+const loggedInFromGoogle = true;
+const loggedInFromEmail = true;
+const guestUser = true;
+// if(loggedInFromEmail || loggedInFromGoogle || guestUser){
+//     console.log("user Logged in");
+// }
---
+++ ControlFlow/thruthy.js
@@ -0,0 +1,6 @@
+const userEmail = "nk@gmail.com"
+// falsy value
+//false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy
+//"0", 'false', " ", [], {}, function(){}
